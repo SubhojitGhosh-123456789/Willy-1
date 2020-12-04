cc87785d4ea2a1e067df13ec9123cbdeee451b5a
SubhojitGhosh-123456789 <subhojit0669@gmail.com>
2020-12-04 16:10:13 +0530

    Modified

--- App.js
+++ App.js
@@ -1,37 +1,35 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import {createAppContainer} from 'react-navigation';
-import {createBottomTabNavigator} from 'react-navigation-tabs';
-import BookTransaction from './Screens/BookTransaction';
-import Search from './Screens/Search';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import BookTransaction from "./Screens/BookTransaction";
+import Search from "./Screens/Search";
 
-export default class App extends React.Component{
-  render(){
-  return (
-    <View style={styles.container}>
-        <AppContainer/>
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <AppContainer />
+      </View>
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 
 const Tab = createBottomTabNavigator({
   Transaction: {
-    screen: BookTransaction
+    screen: BookTransaction,
   },
-  Search:{
-    screen : Search
-  }
+  Search: {
+    screen: Search,
+  },
 });
 
 const AppContainer = createAppContainer(Tab);
-
-
--- Screens/BookTransaction.js
+++ Screens/BookTransaction.js
@@ -1,20 +1,79 @@
-import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import React from "react";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { BarCodeScanner } from "expo-barcode-scanner";
+import * as Permissions from "expo-permissions";
 
 export default class BookTransaction extends React.Component {
-    render(){
-        return(
-            <View style = {styles.container}>
-                <Text>Book Transaction Screen</Text>
-            </View>
-        )
+  constructor() {
+    super();
+    this.state = {
+      output: "",
+      scanned: false,
+      hasCameraPermissions: null,
+      buttonState: "normal",
+    };
+  }
+
+  getCameraPermissions = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA); //Asking Camera Permisions
+    this.setState({
+      hasCameraPermissions: status === "granted",
+      buttonState: "clicked",
+      scanned: "false",
+    });
+  };
+
+  handleBarCodeScanned = async ({ type, data }) => {
+    this.setState({
+      scanned: true,
+      output: data,
+      buttonState: "normal",
+    });
+  };
+
+  render() {
+    const hasCameraPermissions = this.state.hasCameraPermissions;
+    const scanned = this.state.scanned;
+    const buttonState = this.state.buttonState;
+
+    if (buttonState === "clicked" && hasCameraPermissions) {
+      return (
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : this.handleBarCodeScanned}
+          style={StyleSheet.absoluteFillObject}
+        />
+      );
+    } else if (buttonState === "normal") {
+      return (
+        <View style={styles.container}>
+          <Text>Book QR Code Scanner</Text>
+          <Text>
+            {hasCameraPermissions === true
+              ? this.state.output
+              : "Request Camera Permission"}
+          </Text>
+          <TouchableOpacity
+            style={{
+              margin: 50,
+              backgroundColor: "blue",
+              color: "white",
+              fontWeight: "bold",
+              alignItems: "center",
+            }}
+            onPress={this.getCameraPermissions}
+          >
+            <Text> Scan QR Code</Text>
+          </TouchableOpacity>
+        </View>
+      );
     }
+  }
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        alignItems:"center",
-        justifyContent:"center"
-    }
-})
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
